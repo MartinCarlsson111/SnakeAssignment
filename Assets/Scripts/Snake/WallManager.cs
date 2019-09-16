6c6af917b562b3defacc713b231074754b009882
MartinCarlsson11 <martin.carlsson.8580@student.uu.se>
2019-09-16 12:17:14 +0200

    Fixed .gitignore

---
+++ Assets/Scripts/Snake/WallManager.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallManager : MonoBehaviour
+{
+    GameObject wallPrefab;
+    public Vector2Int bounds;
+
+    [SerializeField]
+    List<GameObject> walls;
+
+    [ContextMenu("RemoveWalls")]
+    void RemoveWalls()
+    {
+        foreach (var wall in walls)
+        {
+            DestroyImmediate(wall);
+        }
+        walls.Clear();
+    }
+
+    [ContextMenu("Spawn walls")]
+    void Spawn()
+    {
+        if(walls.Count > 0)
+        {
+            RemoveWalls();
+        }
+
+        wallPrefab = Resources.Load("Prefab/Wall") as GameObject;
+        if (wallPrefab == null)
+        {
+            Debug.Log("Wall prefab could not be found");
+        }
+
+        for (int i = -bounds.x; i < bounds.x; i++)
+        {
+            walls.Add(GameObject.Instantiate(wallPrefab, new Vector2(i + this.transform.position.x, -bounds.y + this.transform.position.y), Quaternion.identity, this.transform));
+        }
+
+        for (int i = -bounds.y; i < bounds.y; i++)
+        {
+            walls.Add(GameObject.Instantiate(wallPrefab, new Vector2(-bounds.x + this.transform.position.x, i + this.transform.position.y), Quaternion.identity, this.transform));
+        }
+
+        for (int i = -bounds.x; i < bounds.x; i++)
+        {
+            walls.Add(GameObject.Instantiate(wallPrefab, new Vector2(i + this.transform.position.x, bounds.y + this.transform.position.y), Quaternion.identity, this.transform));
+        }
+
+        for (int i = -bounds.y; i < bounds.y + 1; i++)
+        {
+            walls.Add(GameObject.Instantiate(wallPrefab, new Vector2(bounds.x + this.transform.position.x, i + this.transform.position.y), Quaternion.identity, this.transform));
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(walls.Count == 0)
+        {
+            Spawn();
+        }
+
+
+    }
+}
---
+++ Assets/Scripts/Snake/WallThatCanKillYou.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallThatCanKillYou : MonoBehaviour
+{
+    public float chance = 0.001f;
+    public float updateTimer = 1.0f;
+    float updateAccu = 0.0f;
+
+    AStarAgent agent;
+    Wall wall;
+    new SpriteRenderer renderer;
+    [SerializeField]
+    public Color col;
+    private void Start()
+    {
+        renderer = GetComponent<SpriteRenderer>();
+        agent = GetComponent<AStarAgent>();
+        wall = GetComponent<Wall>();
+
+        if(wall == null)
+        {
+            Debug.LogError("Game object did not have a wall component");
+        }
+        if(agent == null)
+        {
+            Debug.LogError("Game object did not have an agent component");
+        }
+    }
+
+    private void Update()
+    {
+        updateAccu += Time.deltaTime;
+        if(updateTimer < updateAccu )
+        {
+            var gen = UnityEngine.Random.Range(0.0f, 100.0f);
+            if (gen <= chance)
+            {
+                if (agent != null)
+                {
+                    agent.enabled = true;
+                }
+                if (wall != null)
+                {
+                    wall.Unregister();
+                    wall.enabled = false;
+                }
+                if (renderer != null)
+                {
+                    renderer.color = col;
+                    renderer.sortingOrder = 4;
+                }
+
+                this.enabled = false;
+            }
+            updateAccu = 0;
+        }
+    }
+}
